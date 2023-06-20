181ff8c79bc4db117c84a385939fe08d5870bf1c
Navidarito Chen <Navidarito.Chen@itksquare.edu.mx>
2023-06-20 19:02:57 -0400

    adding_img

--- force-app/main/default/lwc/opportunityWidget/opportunityWidget.js
+++ force-app/main/default/lwc/opportunityWidget/opportunityWidget.js
@@ -1,9 +1,15 @@
 import { LightningElement, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getOpenOpportunities from '@salesforce/apex/OpportunityWidgetController.getOpenOpportunities';
+import sfpic from '@salesforce/resourceUrl/sfpic';
+
 
 export default class OpportunityWidget extends LightningElement {
+    sfimage = sfpic;
+    @track selectedRecordId = [];
     @track data;
     @track columns = [
+        { label: 'Id', fieldName: 'Id', type: 'Id' },
         { label: 'Opportunity Name', fieldName: 'Name', type: 'text' },
         { label: 'Amount', fieldName: 'Amount', type: 'text' },
         { label: 'Close-Won', fieldName: 'Stage', type: 'text' }
@@ -16,5 +22,40 @@ export default class OpportunityWidget extends LightningElement {
         else if(error){
             this.data = undefined;
         }
+    }
+
+    handleSingleCheckboxSelect(event) {
+        const boxes = this.template.querySelectorAll('lightning-input[data-key="singleSelectColumnCheckbox"]');
+
+        boxes.forEach(box =>{
+            if(box.type =='checkbox' && box.checked){
+                
+                this.selectedRecordId(box.value);
+                
+            }
+
+            
+        });
+
+        if(this.selectedRecordId!=null){
+            console.log(this.selectedRecordId);
+            try {
+                this.showToast();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        
+    }
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Toast message',
+            message: 'Congratulations!! '+this.selectedRecordId,
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        console.log(this.sfimage);
+        this.dispatchEvent(event);
     }
 }
